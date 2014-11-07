84d52fa280ea4dafb25838eec4522672c0ea0d12
axtheset <sethaxthelm@gmail.com>
2014-11-07 10:44:42 -0500

    Initial commit

---
+++ assets/_vti_cnf/jquery.dynatable.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|CHARCO\\woodruffd
+vti_modifiedby:SR|CHARCO\\woodruffd
+vti_timelastmodified:TR|16 Sep 2014 12:03:16 -0000
+vti_timecreated:TR|16 Sep 2014 12:03:16 -0000
+vti_cacheddtm:TX|16 Sep 2014 12:03:16 -0000
+vti_filesize:IR|58666
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|index.html
---
+++ assets/js/modules/_vti_cnf/canvas-tools.src.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|CHARCO\\woodruffd
+vti_modifiedby:SR|CHARCO\\woodruffd
+vti_timelastmodified:TR|03 Nov 2014 16:24:16 -0000
+vti_timecreated:TR|03 Nov 2014 16:24:16 -0000
+vti_cacheddtm:TX|03 Nov 2014 16:24:16 -0000
+vti_filesize:IR|100677
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
